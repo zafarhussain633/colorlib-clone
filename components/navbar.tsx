24cad16aebe1829e80c77dc59ba76a8c8ac552b2
zafar <zafarhussain633@gmail.com>
2021-07-15 16:12:36 +0530

    moved cdnlink to app file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,20 +7,6 @@ import Head from 'next/head'
 const Navbar: FC = () => {
   const [state, setstate] = useState<boolean>(true);
   return (
-
-    <>
-     <header>
-        <Head>
-        <title>simple next.js app by_zafar</title>
-        <meta name="keyword" content="next.js"/>
-         {/* bootstrap cdn*/}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
-         {/* font awsome cdn */}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        </Head>
-      </header>
-
     <nav className={styles.navbar_postion}>
       {state ? (
         <Defaultnavbar handleclick={() => setstate(!state)} />
@@ -29,7 +15,6 @@ const Navbar: FC = () => {
       )}
       <NavbarWithHamburger />
     </nav>
-    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import Head from "next/head";
+
 import Mainpage from "../components/mainpage/mainpage";
-import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (

8a0178b57fa91e79d43393918455d2d3267626ac
zafar <zafarhussain633@gmail.com>
2021-07-15 15:56:01 +0530

    deleted head file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from "react";
 import styles from "../styles/Navbar.module.scss";
 import Link from "next/link";
-import Headers from "./head";
+import Head from 'next/head'
 
 
 const Navbar: FC = () => {
@@ -9,9 +9,17 @@ const Navbar: FC = () => {
   return (
 
     <>
-    <header>
-      <Headers />
-    </header>
+     <header>
+        <Head>
+        <title>simple next.js app by_zafar</title>
+        <meta name="keyword" content="next.js"/>
+         {/* bootstrap cdn*/}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+         {/* font awsome cdn */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        </Head>
+      </header>
 
     <nav className={styles.navbar_postion}>
       {state ? (
